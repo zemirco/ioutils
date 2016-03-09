0cbb32ff7c6137373e99170540fe22ee593aa62e
Mirco Zeiss <mirco.zeiss@gmail.com>
2016-03-09 07:38:09 +0100

    add example for readAllTrim

--- ioutils_test.go
+++ ioutils_test.go
@@ -2,6 +2,7 @@ package ioutils
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -24,6 +25,24 @@ func TestReadAllTrim(t *testing.T) {
 	if !bytes.Equal(out, expected) {
 		t.Errorf("got %v but expected %v", out, expected)
 	}
+}
+
+func ExampleReadAllTrim() {
+	in := strings.NewReader(`
+		<html>
+			<head>
+			</head>
+			<body>
+				<p>hello, world!</p>
+			</body>
+		</html>
+	`)
+	out, err := ReadAllTrim(in)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(out))
+	// Output: <html><head></head><body><p>hello, world!</p></body></html>
 }
 
 func BenchmarkReadAllTrim(b *testing.B) {
